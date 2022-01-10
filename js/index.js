2deb535c77b775b26b0fea5addce3309af8cb6d2
Fred Awumey <fredawumeyfafa@gmail.com>
2022-01-10 16:29:38 +0000

    update email data collected successfully

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var form = document.getElementById('db-form');
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("db-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      alert('Successful')
+      window.location.href = 'https://remysportfolio.netlify.app/';
+    });
+  });
+
+
+
+
+function save() 
+{
+    const data = document.getElementById('db-form').elements[0].value;
+ 
+  if(ValidateEmail(data))
+  {
+    database.ref('email').push({ email: data}).then(function(snapshot) {
+      pass(); // some success method
+      }, function(error) 
+         {
+            console.log('error' + error);
+            fail(); // some error method
+          });
+     document.getElementById('frm1').reset();
+  }
+
+}
+
+// function ValidateEmail(mail) 
+// {
+//     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail))
+//         {
+//           pass();
+//           return (true);
+//         }
+//     else{
+//           fail();
+//           return (false);
+//           }
+// } 
+
+
+// function fail()
+// {
+
+//   $('#fail').finish().show().delay(1000).fadeOut(6000);
+
+// }
+// function pass()
+// {
+ 
+// $('#succ').finish().show().delay(1000).fadeOut(6000);
+
+// }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '48f2362416fd129377a8f9b24a8d9d05fa54f426',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '48f2362416fd129377a8f9b24a8d9d05fa54f426',
+            'dev_requirement' => false,
+        ),
+        'sheetdb/sheetdb-php' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sheetdb/sheetdb-php',
+            'aliases' => array(),
+            'reference' => '1cc8d76e6377adcce71f8c58f3de1f2e3a146e8d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
