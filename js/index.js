893e463491915e23a1fb1eeb8fab482134653ac1
Fred Awumey <fredawumeyfafa@gmail.com>
2022-01-12 07:03:17 +0000

    Alert-box styled

--- js/index.js
+++ js/index.js
@@ -8,54 +8,8 @@ var form = document.getElementById('db-form');
         response => response.json()
     ).then((html) => {
       // you can put any JS code here
-      alert('Successful')
+      swal("Good job!", "You clicked the button!", "success")
+      // alert('Successful')
+      // window.location.href = 'https://abc.com';
     });
   });
-
-
-
-
-// function save() 
-// {
-//     const data = document.getElementById('db-form').elements[0].value;
- 
-//   if(ValidateEmail(data))
-//   {
-//     database.ref('email').push({ email: data}).then(function(snapshot) {
-//       pass(); // some success method
-//       }, function(error) 
-//          {
-//             console.log('error' + error);
-//             fail(); // some error method
-//           });
-//      document.getElementById('frm1').reset();
-//   }
-
-// }
-
-// function ValidateEmail(mail) 
-// {
-//     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail))
-//         {
-//           pass();
-//           return (true);
-//         }
-//     else{
-//           fail();
-//           return (false);
-//           }
-// } 
-
-
-// function fail()
-// {
-
-//   $('#fail').finish().show().delay(1000).fadeOut(6000);
-
-// }
-// function pass()
-// {
- 
-// $('#succ').finish().show().delay(1000).fadeOut(6000);
-
-// }
