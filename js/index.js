00894f27de05a45dee14fce7d75341d265d99cc3
Fred Awumey <fredawumeyfafa@gmail.com>
2022-01-12 17:42:14 +0000

    Button edited

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
 var form = document.getElementById('db-form');
   form.addEventListener("submit", e => {
+   var button = document.getElementById("submitBtn");
+    button.innerText = 'Submitting';
+    button.disabled = true;
+    button.style.backgroundColor = "#adadad";
+
     e.preventDefault();
+  
     fetch(form.action, {
         method : "POST",
         body: new FormData(document.getElementById("db-form")),
     }).then(
         response => response.json()
     ).then((html) => {
-      // you can put any JS code here
+ 
+      // document.getElementById("loadingIcon").style.display = 'none';
+      button.style.backgroundColor = "#6db983";
+      button.innerText = 'Join Waitlist';
+      button.disabled = false;
       swal("Good job!", "Successful!", "success")
       document.getElementById("db-form").reset();
       // window.location.href = 'https://abc.com';
     });
   });
-
-  function change() // no ';' here
-{
-    var elem = document.getElementById("submitBtn");
-    if (elem.value=="Submitting") elem.value = "Join Waitlist";
-    else elem.value = "Submitting";
-}
 
